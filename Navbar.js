84b78ae5f7b63a8f8f1e4ca5f30d5f6395a48bbf
Evan Buell <buell-e@webmail.uwinnipeg.ca>
2021-03-19 13:09:44 -0500

    test

--- Navbar.js
+++ Navbar.js
@@ -7,7 +7,5 @@ $(document).on("scroll", ()=>{
     $("#self-image").height(a);
 });
 
-$( document ).ready(function() {
     $("#header").load("header.html"); 
     $("#footer").load("footer.html"); 
-});
