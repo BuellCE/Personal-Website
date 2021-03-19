d742ab13031e90efabb6ba5a2755327a670ef186
Evan Buell <buell-e@webmail.uwinnipeg.ca>
2021-03-19 13:31:07 -0500

    fixed nav

--- Navbar.js
+++ Navbar.js
@@ -1,13 +1,18 @@
 $(document).on("scroll", ()=>{
+    resizeTopBar();
+});
+
+$(document).ready(function() {
+    resizeTopBar();
+    $("#header").load("header.html"); 
+    $("#footer").load("footer.html"); 
+});
+
+function resizeTopBar(){
     var a = 110 - $(document).scrollTop();
     if (a < 70){
         a = 70;
     }
     $("#self-image").width(a);
     $("#self-image").height(a);
-});
-
-$(document).ready(function() {
-    $("#header").load("header.html"); 
-    $("#footer").load("footer.html"); 
-});
+}
